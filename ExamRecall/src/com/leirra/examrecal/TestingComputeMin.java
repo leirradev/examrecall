415860a582ccde2b62c6b52d812badf39cba1365
lstrmchgn <lestermiro.lm@gmail.com>
2019-03-06 20:36:31 +0800

    second commit and push

--- ExamRecall/src/com/leirra/examrecal/TestingComputeMin.java
+++ ExamRecall/src/com/leirra/examrecal/TestingComputeMin.java
@@ -3,13 +3,20 @@ package com.leirra.examrecal;
 import java.util.Arrays;
 
 public class TestingComputeMin {
-
+//	Output
+//	test case:5
+//	An Array to print and repeat its element is the number is 5
+//	Answer: [5,5,5,5,5]
+//	Conclusion: Not yet done
 	public static void main(String[] args) {
 		TestingComputeMin e = new TestingComputeMin();
 		int num = 3;
-		System.out.println("An Array to print and repeat its element is the number " + num + " \nAnswer: "
-				+ Arrays.toString(e.solution(num)));
 		int[] f = { 5, 4, 5, 6, 3 };
+		int min_num = e.find_min(f);
+		int[] arr = e.solution(min_num);
+
+		System.out.println(
+				"An Array to print and repeat its element is the number " + num + " \nAnswer: " + Arrays.toString(arr));
 		System.out.println(e.find_min(f));
 	}
 
